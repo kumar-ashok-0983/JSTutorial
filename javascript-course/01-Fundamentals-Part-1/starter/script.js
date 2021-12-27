f5dbb7a7089c1a96dc004144c9a98271d2c1912e
Ashok <kumar.ashok@tide.co>
2021-12-27 21:06:19 +0000

    fundamentals part 1 updated

--- javascript-course/01-Fundamentals-Part-1/starter/script.js
+++ javascript-course/01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,4 @@
+/*
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
 
@@ -10,10 +11,47 @@ console.log(firstName);
 console.log(firstName);
 console.log(firstName);
 
-let country = "United Kingdom"
-let continent = "Europe"
-let population = 80
+*/
 
-console.log(country)
-console.log(continent)
-console.log(population)
+// LECTURE: Values and Variables
+let country = "United Kingdom";
+let continent = "Europe";
+let population = 80;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+/*
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+console.log(typeof true);
+console.log(typeof javascriptIsFun);
+console.log(typeof 23);
+console.log(typeof "string");
+
+javascriptIsFun = "Yes!";
+console.log(typeof javascriptIsFun);
+
+
+let year;
+console.log(year)
+console.log(typeof year)
+
+year = 1991
+console.log(year)
+console.log(typeof year)
+
+console.log(typeof null)
+*/
+
+// LECTURE: Data Types
+let isIsland = true;
+let language;
+
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
