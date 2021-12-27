ca0e1bb623b512634cee0171aadd2cd93e5dd483
Ashok <kumar.ashok@tide.co>
2021-12-27 19:54:49 +0000

    fundamentals part 1 updated

---
+++ javascript-course/01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,19 @@
+let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log('Ashok');
+console.log(23);
+
+let firstName = 'Jenny';
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let country = "United Kingdom"
+let continent = "Europe"
+let population = 80
+
+console.log(country)
+console.log(continent)
+console.log(population)
