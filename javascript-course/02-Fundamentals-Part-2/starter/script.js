aefece91f7904624bfa0f1885dd45a5039678185
Ashok <kumar.ashok@tide.co>
2022-01-02 19:35:28 +0000

    updated lecture pratices

--- javascript-course/02-Fundamentals-Part-2/starter/script.js
+++ javascript-course/02-Fundamentals-Part-2/starter/script.js
@@ -164,3 +164,82 @@ if (objectMark.calcBMI() > objectJohn.calcBMI()) {
     console.log(`${objectJohn.fullName}'s BMI (${objectJohn.bmi}) is equal to the ${objectMark.fullName}'s BMI (${objectMark.bmi})`)
 }
 */
+
+/*
+for (let rep = 1; rep <= 10; rep++) {
+    console.log(`Lifting weights repitition ${rep}`);
+}
+*/
+
+/*
+const ashok = [
+    "Ashok",
+    "Mishra",
+    2021 - 1983,
+    'Engineer',
+    ['Jay', 'Shilpi', 'Shrinjal']
+];
+
+const types = [];
+
+for (let i = 0; i < ashok.length; i++) {
+    console.log(ashok[i], typeof ashok[i]);
+    types.push(typeof ashok[i]);
+}
+
+console.log(types);
+
+for (let i = 0; i < ashok.length; i++) {
+    if (typeof ashok[i] !== 'string') continue;
+    console.log(ashok[i], typeof ashok[i]);
+}
+
+for (let i = 0; i < ashok.length; i++) {
+    if (typeof ashok[i] === 'number') break;
+    console.log(ashok[i], typeof ashok[i]);
+}
+
+*/
+
+/*
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    dice = Math.trunc(Math.random() * 6) + 1;
+    if (dice === 6) console.log(`Loop is about to end...`);
+}
+
+*/
+
+/*
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+
+let tips = [];
+let totals = [];
+
+const calcTip = function (bill) {
+    return bill <= 300 && bill >= 50 ? bill * 0.15 : bill * 0.20;
+}
+
+
+const calcAvg = function (arr) {
+    let sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum = sum + arr[i];
+    }
+    return sum / arr.length;
+}
+
+for (let i = 0; i < bills.length; i++) {
+    tips.push(calcTip(bills[i]));
+    totals.push(bills[i] + tips[i]);
+
+    console.log(`Bill amount: ${bills[i]}, tip : ${tips[i]}, total amount to pay: ${totals[i]}`);
+}
+
+console.log(calcAvg(bills));
+console.log(calcAvg(tips));
+console.log(calcAvg(totals));
+*/
