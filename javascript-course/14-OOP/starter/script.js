d3eb128e84b0674464a8b6d3ca1d60ff14d6614d
Shilpi Aggarwal <shilpi275@gmail.com>
2022-01-05 00:30:41 +0000

    Object lectures updates

--- javascript-course/14-OOP/starter/script.js
+++ javascript-course/14-OOP/starter/script.js
@@ -44,8 +44,17 @@ const arr = [3, 6, 4, 5, 6, 9, 3];
 console.log(arr.__proto__ === Array.prototype);
 
 console.log(arr.__proto__.__proto__);
+
+Person.hey = function () {
+  console.log('Hey there, what up?');
+  console.log(this);
+};
+
+Person.hey();
+
 */
 
+/*
 const Car = function (make, speed) {
   this.make = make;
   this.speed = speed;
@@ -70,3 +79,67 @@ bmw.accelerate();
 bmw.accelerate();
 bmw.brake();
 bmw.accelerate();
+
+*/
+
+/*
+const accounts = {
+  owner: 'Ashok',
+  movements: [200, 530, 120, 300],
+
+  get latest() {
+    return this.movements.slice(-1).pop();
+  },
+
+  set latest(mov) {
+    this.movements.push(mov);
+  },
+};
+
+console.log(accounts.latest);
+accounts.latest = 50;
+console.log(accounts.movements);
+*/
+
+class PersonCl {
+  constructor(fullName, birthYear) {
+    this.fullName = fullName;
+    this.birthYear = birthYear;
+  }
+
+  calcAge() {
+    console.log(2021 - this.firstName);
+  }
+
+  greet() {
+    console.log(`hey ${this.firstName}`);
+  }
+
+  get age() {
+    return 2021 - this.birthYear;
+  }
+
+  set fullName(name) {
+    if (name.includes(' ')) this._fullName = name;
+    else alert(`${name} is not full name`);
+  }
+
+  get fullName() {
+    return this._fullName;
+  }
+
+  static hey() {
+    console.log('Hey whats up');
+    console.log(this);
+  }
+}
+
+const shilpi = new PersonCl('Shilpi Aggarwal', 1984);
+console.log(shilpi);
+console.log(shilpi.age);
+
+console.log(shilpi.__proto__ === PersonCl.prototype);
+
+const walter = new PersonCl('Walter White', 1965);
+
+PersonCl.hey();
